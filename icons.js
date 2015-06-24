a17c4b613728d67d17daa393388a47682e3d8253
mapsam <matthews.sam@gmail.com>
2015-06-24 12:12:43 -0400

    adding some grids

--- icons.js
+++ icons.js
@@ -13,12 +13,24 @@ jf.spaces = 2;
 
 fs.readdir('src/', function (err, files) {
   var json = { version: pkg.version, files: [] };
+
   for (var i = 0; i < files.length; i++) {
-    if (files[i].indexOf('.svg') !== -1) json.files.push(files[i]);
+    if (files[i].indexOf('.svg') !== -1) {
+      var file = {
+        name: files[i].slice(0, -4),
+        svg: files[i]
+      };
+      json.files.push(file);
+    }
   }
+
+  
   jf.writeFile('icons.json', json, function(err) {
     if (err) {
       return console.log(err);
     }
   });
+
+
+
 });
